4b2769b7b1c58f3b129183f5147c83deb5320f1a
Ghoort <ghoort1@gmail.com>
2016-10-11 20:22:23 +0300

    Update GoogleTokens.h

--- GoogleTokens.h
+++ GoogleTokens.h
@@ -1,6 +1,6 @@
 #pragma once
 
 
-static const char api_key[] = "AIzaSyBxNOHAcOOkEymgcgopdLGxm9rkAEpw21Q";
-static const char client_id[] = "645716808396-j2o935mog2uflt5q97if5r7i5e3p7vg9.apps.googleusercontent.com";
-static const char client_secret[] = "A8aVOZlW9-MWeCJVL8dL2FIP";
+static const char api_key[] = "insert your API key";
+static const char client_id[] = "your client id";
+static const char client_secret[] = "and client secret";
